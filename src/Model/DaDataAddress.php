e531bd358b2d40927f01c354c244e62188016071
slavaveremeevskii <vveremeevskii@wowworks.ru>
2019-11-15 19:06:54 +0400

    add DaData

---
+++ src/Model/DaDataAddress.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Dadata\Model;
+
+use Geocoder\Model\Address;
+
+/**
+ * Class DaDataAddress
+ * @package common\components\dadata\Model
+ */
+class DaDataAddress extends Address
+{
+
+}
