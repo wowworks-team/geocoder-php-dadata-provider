d5b2a456005bfdcb13e12e4a13a29ab489db1f4e
slavaveremeevskii <vveremeevskii@wowworks.ru>
2019-11-19 10:01:53 +0400

    update composer, unitest

--- test/DaDataTest.php
+++ test/DaDataTest.php
@@ -1,79 +1,29 @@
 <?php
 
-
 class DaDataTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @var \DadataSuggestions\DadataSuggestionsService
-     */
-    private $service;
-
-    /**
-     * @var \Dadata\DaData
+     * @var \Dadata\DaData | \PHPUnit\Framework\MockObject\MockObject
      */
     private $daData;
 
     public function setUp(): void
     {
         parent::setUp();
-
-        $this->service = $this->getService();
-        $this->daData = $this->getDaData($this->service);
+        $this->daData = $this->createMock(\Dadata\DaData::class);
     }
 
-
-    public function testDaDataTypes()
+    public function testGeocodeQuery()
     {
-        $data = $this->daData->geocodeQuery(\Geocoder\Query\GeocodeQuery::create('Moscow'));
 
-        $this->assertInstanceOf(\Geocoder\Model\AddressCollection::class, $data);
+        $this->assertInstanceOf(\Dadata\DaData::class, $this->daData);
+        $geocodeQuery = \Geocoder\Query\GeocodeQuery::create('г Москва, улица Академика Королева, дом 15, корп. 2');
 
-        foreach ($data as $key => $item) {
-            $this->assertInstanceOf(\Dadata\Model\DaDataAddress::class, $item);
-        }
-    }
+        $data = $this->daData->method('geocodeQuery')
+            ->with($geocodeQuery)
+            ->will($this->returnValue(\Geocoder\Collection::class));
 
-    public function testDaData()
-    {
-        $query = \Geocoder\Query\GeocodeQuery::create('г Москва, улица Академика Королева, дом 15, корп. 2');
-        $data = $this->daData->geocodeQuery($query);
 
-        $this->assertNotEmpty($data);
-
-        $daDataAddres = array_pop($data->all());
-
-        $this->assertNotEmpty($daDataAddres->getCoordinates());
-
-        $this->assertEquals($daDataAddres->getStreetName(), 'Академика Королева');
-
-        $this->assertEquals($daDataAddres->getStreetNumber(), 15);
-
-        $this->assertEquals($daDataAddres->getSubLocality(), 'г Москва');
-
-        $this->assertEquals($daDataAddres->getLocality(), 'г Москва');
-
-        $this->assertObjectHasAttribute('name', $daDataAddres->getCountry());
-
-        $this->assertObjectHasAttribute('code', $daDataAddres->getCountry());
-
-    }
-
-    /**
-     * @return \DadataSuggestions\DadataSuggestionsService
-     */
-    private function getService()
-    {
-        $service = new \DadataSuggestions\DadataSuggestionsService();
-        $service->setToken('ab626ac37b47868748ea2f408293ed6a1e420944');
-        return $service;
-    }
-
-    /**
-     * @param \DadataSuggestions\DadataSuggestionsService $service
-     * @return \Dadata\DaData
-     */
-    private function getDaData(\DadataSuggestions\DadataSuggestionsService $service)
-    {
-        return new \Dadata\DaData($service);
+        $this->assertIsObject($data);
     }
 }
