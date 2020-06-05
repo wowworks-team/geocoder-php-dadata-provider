75759e54b268cb6374e23011739a8af5058b79d3
Victor Demin <mail@vdemin.com>
2020-06-05 16:08:37 +0400

    Update DaDataAddress.php

--- src/Model/DaDataAddress.php
+++ src/Model/DaDataAddress.php
@@ -3,6 +3,7 @@
 namespace Wowworks\Dadata\Model;
 
 use Geocoder\Model\Address;
+use Geocoder\Provider\Yandex\Model\YandexAddress;
 
 /**
  * Class DaDataAddress
@@ -10,5 +11,47 @@ use Geocoder\Model\Address;
  */
 class DaDataAddress extends Address
 {
+    /*
+https://dadata.ru/suggestions/usage/address/
+0 — точные координаты
+1 — ближайший дом
+2 — улица
+3 — населенный пункт
+4 — город
+5 — координаты не определены
+     */
+    const QC_GEO_ACCURATE = 0;
+    const QC_GEO_HOUSE = 1;
+    const QC_GEO_STREET = 2;
+    const QC_GEO_SETTLEMENT = 3;
+    const QC_GEO_CITY = 4;
+    const QC_GEO_UNKNOWN = 5;
 
+    /**
+     * @var int|null
+     */
+    private $precision;
+
+    /**
+     * See https://dadata.ru/suggestions/usage/address/ data.qc_geo
+     *
+     * @return null|int
+     */
+    public function getPrecision()
+    {
+        return $this->precision;
+    }
+
+    /**
+     * @param null|int $precision
+     *
+     * @return DaDataAddress
+     */
+    public function withPrecision(int $precision = null): self
+    {
+        $new = clone $this;
+        $new->precision = $precision;
+
+        return $new;
+    }
 }
